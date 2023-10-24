b2b96f78f368d0df1328096898b9e3c1362a43f2
Dinesh-Singh-au51 <dineshthakur14@gmail.com>
2023-10-24 12:07:30 +0530

    7th commit folder deleted

--- input.js
+++ input.js
@@ -1,2 +1,13 @@
-n=require ("prompt-sync")()();
-console.log(n)
+a=10
+b=15.5
+c="hello"
+d=NaN
+var e
+var f=null
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+console.log(typeof e)
+console.log(typeof f)
+console.log(a)
